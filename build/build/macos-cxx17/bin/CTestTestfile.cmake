c74397a98e49946d88c20f8da2c6120a691810f7
Lambourl <lmbrluc@gmail.com>
2022-09-06 14:47:37 +0200

    :wink: add file and build folder

---
+++ build/build/macos-cxx17/bin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/luclambour/.tipi/w/unit-test-project-compile-2e803a6/build/macos-cxx17/recipe
+# Build directory: /Users/luclambour/.tipi/w/unit-test-project-compile-2e803a6/build/macos-cxx17/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "plain unmodified unit-test-project" << std::endl;
+  return 0;
+}
