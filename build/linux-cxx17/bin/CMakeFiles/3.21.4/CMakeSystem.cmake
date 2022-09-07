9a0fbece9dd45b1d5521c0a192993560ebc26f16
Lambourl <lmbrluc@gmail.com>
2022-09-07 17:40:40 +0200

    :wink: add linux build folder

---
+++ build/linux-cxx17/bin/CMakeFiles/3.21.4/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.4.0-121-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-121-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/root/.tipi/w/unit-test-project-compile-2e803a6/build/linux-cxx17/installed/proxy.cmake")
+
+set(CMAKE_SYSTEM "Linux-5.4.0-121-generic")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "5.4.0-121-generic")
+set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
+set(CMAKE_CROSSCOMPILING "FALSE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ build/linux-cxx17/bin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/.tipi/w/unit-test-project-compile-2e803a6/build/linux-cxx17/recipe
+# Build directory: /root/.tipi/w/unit-test-project-compile-2e803a6/build/linux-cxx17/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
